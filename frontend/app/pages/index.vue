3429c186caffb71938667e21dbd24d683ca04770
david <davidmyatminhan123@gmail.com>
2026-04-23 21:02:11 +0800

    added img, changed index file, recoded the categories section

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,15 +1,33 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const navLinks = ref(['Textbooks', 'Electronics', 'Services', 'Campus Gear'])
+  const searchText = ref('')
+
+  const featuredTitle = ref('Featured Picks')
+  const products = ref([
+    { name: 'Calculator', price: 'RM 145.00' },
+    { name: 'Keyboard', price: 'RM 210.00' },
+    { name: 'Notebook', price: 'RM 45.00' }
+  ])
+
+  const ctaTitle = ref('Your unused gear could help others')
+  const ctaText = ref('Turn materials into funds.')
+  const ctaButton = ref('Post Your First Listing')
+</script>
+
 <template>
   <div class="max-w-300 mx-auto px-4">
 
     <!-- Navbar -->
     <div class="flex justify-between items-center py-4">
-      <div class="text-xl font-bold">{{ siteName }}</div>
+      <div class="text-xl font-bold text-primary">MMUMART</div>
 
       <div class="flex gap-4">
         <a
           v-for="link in navLinks"
           :key="link"
-          class="text-gray-700 hover:text-blue-600 cursor-pointer"
+          class="text-gray-500 hover:text-primary cursor-pointer"
         >
           {{ link }}
         </a>
@@ -19,37 +37,67 @@
         v-model="searchText"
         type="text"
         placeholder="Search..."
-        class="border rounded px-3 py-1 outline-none focus:ring-2 focus:ring-blue-400"
+        class="border rounded px-3 py-1 outline-none focus:ring-1 focus:ring-primary"
       />
     </div>
 
     <!-- Hero -->
-    <div class="bg-blue-900 text-white p-10 rounded-xl mt-4">
-      <h1 class="text-3xl font-bold mb-2">{{ heroTitle }}</h1>
-      <p class="mb-4">{{ heroSubtitle }}</p>
+    <div class="bg-primary text-white p-10 rounded-xl mt-4">
+      <h1 class="text-4xl font-bold mb-2">Discover Quality Campus Essentials.</h1>
+      <p class="mb-4 text-gray-500">Buy, sell, and trade academic materials.</p>
 
-      <button class="bg-white text-blue-900 px-4 py-2 rounded mr-2">
-        {{ heroBtn1 }}
+      <button class="bg-white text-primary px-4 py-2 rounded-md mr-2 text-sm font-medium">
+        Start Exploring
       </button>
 
-      <button class="bg-blue-700 px-4 py-2 rounded">
-        {{ heroBtn2 }}
+      <button class="bg-blue-700 px-4 py-2 rounded-md text-sm font-medium">
+        Become a Seller
       </button>
     </div>
 
     <!-- Categories -->
     <div class="mt-8">
-      <h2 class="text-xl font-semibold mb-4">{{ categoryTitle }}</h2>
-
-      <div class="grid grid-cols-4 gap-4">
-        <div
-          v-for="cat in categories"
-          :key="cat"
-          class="bg-white p-4 rounded-lg shadow text-center hover:shadow-md"
-        >
-          {{ cat }}
+      <section class="flex items-center justify-between mb-5">
+        <div>
+          <h2 class="text-xl font-semibold text-primary">Academic Categories</h2>
+          <p class=" text-gray-500 text-sm">Browse collections by campus department.</p>
         </div>
-      </div>
+        <div>
+          <a href="/categories" class="font-medium hover:text-primary text-sm hover:underline">View All Categories</a>
+        </div>
+      </section>
+      <section class="grid grid-cols-2 gap-2 ">
+        <div class="relative h-100">
+          <img src="/img/textbooks.png" alt="textbooks" class="h-full w-full rounded-lg">
+          <div class="absolute inset-0 bg-black/40 rounded-lg"></div>
+          <div class="absolute bottom-0 w-full text-white p-5 rounded-b-lg">
+            <h1 class="text-2xl font-semibold">Textbooks</h1>
+          </div>
+        </div>
+        <div class="grid grid-cols-2 gap-2">
+            <div class="relative h-50">
+              <img src="/img/electronics.png" alt="electronics" class="h-full w-full  rounded-lg">
+              <div class="absolute inset-0 bg-black/40 rounded-lg"></div>
+              <div class="absolute bottom-0 w-full text-white p-2 rounded-b-lg">
+                <h1 class="text-lg font-semibold">Electronics</h1>
+              </div>
+            </div>
+            <div class="relative h-50">
+              <img src="/img/stationary.png" alt="stationary" class="h-full w-full rounded-lg">
+              <div class="absolute inset-0 bg-black/40 rounded-lg"></div>
+              <div class="absolute bottom-0 w-full text-white p-2 rounded-b-lg">
+                <h1 class="text-lg font-semibold">Stationary</h1>
+              </div>
+            </div>
+            <div class="relative col-span-2 h-48">
+              <img src="/img/campus_gear.png" alt="camppus_gear" class="h-full w-full object-cover rounded-lg">
+              <div class="absolute inset-0 bg-black/40 rounded-lg"></div>
+              <div class="absolute bottom-0 w-full text-white p-2 rounded-b-lg">
+                <h1 class="text-lg font-semibold">Campus Gear</h1>
+              </div>
+            </div>
+        </div>
+      </section>
     </div>
 
     <!-- Products -->
@@ -81,30 +129,3 @@
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-
-const siteName = ref('MMUMART')
-
-const navLinks = ref(['Textbooks', 'Electronics', 'Services', 'Campus Gear'])
-const searchText = ref('')
-
-const heroTitle = ref('Discover Quality Campus Essentials.')
-const heroSubtitle = ref('Buy, sell, and trade academic materials.')
-const heroBtn1 = ref('Start Exploring')
-const heroBtn2 = ref('Become a Seller')
-
-const categoryTitle = ref('Academic Categories')
-const categories = ref(['Textbooks', 'Electronics', 'Services', 'Campus Gear'])
-
-const featuredTitle = ref('Featured Picks')
-const products = ref([
-  { name: 'Calculator', price: 'RM 145.00' },
-  { name: 'Keyboard', price: 'RM 210.00' },
-  { name: 'Notebook', price: 'RM 45.00' }
-])
-
-const ctaTitle = ref('Your unused gear could help others')
-const ctaText = ref('Turn materials into funds.')
-const ctaButton = ref('Post Your First Listing')
-</script>
